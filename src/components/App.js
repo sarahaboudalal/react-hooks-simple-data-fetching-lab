4dfbab436dbaab6efcf3632a019d52dfb837f4f5
Sarah Abou Dalal <sarahaboudalal@gmail.com>
2022-09-23 22:08:03 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,19 @@
 // create your App component here
+import {useEffect, useState} from "react"
+
+export default function App(){
+const [randomImage, setRandomImage] = useState()
+const [isLoaded, setIsLoaded] = useState(false);
+
+useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+    .then((response)=> response.json())
+    .then((data) => { 
+    setRandomImage(data.message)
+    setIsLoaded(true)
+    })
+}, [])
+ if (!isLoaded) return <p>Loading...</p>;
+
+ return <img src={randomImage} alt="A Random Dog"/>
+}
